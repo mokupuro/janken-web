45c0e60af202a26329e7591b21bd7d2204beb25e
soheyHashimoto <heyso1209@gmail.com>
2022-06-24 02:02:39 +0900

    [ADD]ボタンを表示させる

--- src/components/select_button.rs
+++ src/components/select_button.rs
@@ -1,19 +1,24 @@
 
 use yew::{function_component, html, Properties, Callback};
 
+#[derive(Properties, PartialEq)]
+pub struct SelectButtonProps {
+    pub selected: yew::UseStateHandle<i32>,
+}
+
 #[function_component(SelectButton)]
-pub fn select_button() -> Html {
+pub fn select_button(props: &SelectButtonProps) -> Html {
     let onclick = {
         let selected = props.selected.clone();
-        let content  = props.content.clone();
-        Callback::from(move |_| selected.set(content.clone()))
+        Callback::from(move |_| selected.set(0))
     };
 
     html! {
-      <act_style_button
+      <button
         class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+        { onclick }
       >
         { "グー" }
-      </act_style_button>
+      </button>
     }
 }
