8f707d9a3f9c7fc2e84d328db5a539b0fec70540
soheyHashimoto <heyso1209@gmail.com>
2022-06-24 02:02:28 +0900

    [ADD]select_buttonを用いる

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+mod components;
+
 use yew::prelude::*;
+use components::select_button::SelectButton;
 
 #[function_component(App)]
 fn app() -> Html {
+    let selected = use_state(|| 0);
+
     html! {
-        <div>
-            <h1 class="text-3xl font-bold text-center ">{"Hello, world!"}</h1>
+        <div class="flex justify-center mt-3">
+            <SelectButton selected={ selected.clone() } />
         </div>
     }
 }
